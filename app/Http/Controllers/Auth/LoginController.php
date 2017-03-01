9009d0d5f4da66e389e6984e1a4b9292db8474fe
arsibux <arsibux@gmail.com>
2017-03-01 11:58:49 +0500

    package

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,4 +45,26 @@ use AuthenticatesUsers;
             }
         }
     }
+     public function package_validity($validity) {
+        $package_validity = array();
+        switch ($validity) {
+            case 1:
+                $package_validity['package'] = "Trial Package";
+                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
+                break;
+            case 3:
+                $package_validity['package'] = "Silver Package";
+                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
+                break;
+            case 6:
+                $package_validity['package'] = "Gold Package";
+                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
+                break;
+            case 12:
+                $package_validity['package'] = "Classic Package";
+                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
+                break;
+        }
+        return $package_validity;
+    }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,23 +6,21 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use App\Models\Package;
-use Carbon\Carbon;
-use App\Models\Subscription;
 
-class RegisterController extends Controller {
+class RegisterController extends Controller
+{
     /*
-      |--------------------------------------------------------------------------
-      | Register Controller
-      |--------------------------------------------------------------------------
-      |
-      | This controller handles the registration of new users as well as their
-      | validation and creation. By default this controller uses a trait to
-      | provide this functionality without requiring any additional code.
-      |
-     */
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
 
-use RegistersUsers;
+    use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
@@ -36,7 +34,8 @@ use RegistersUsers;
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('guest');
     }
 
@@ -46,12 +45,12 @@ use RegistersUsers;
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data) {
+    protected function validator(array $data)
+    {
         return Validator::make($data, [
-                    'name' => 'required|max:255',
-                    'email' => 'required|email|max:255|unique:users',
-                    'password' => 'required|min:6|confirmed',
-                    'subscription' => 'required',
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
         ]);
     }
 
@@ -61,53 +60,12 @@ use RegistersUsers;
      * @param  array  $data
      * @return User
      */
-    protected function create(array $data) {
-
-        $package_id = $data['subscription'];
-        $package = Package::find($package_id);
-//        dd($package);exit;
-        $package_ends_at = $this->package_validity($package->validity);
-
-//        dd($package_ends_at);exit;
-         User::create([
+    protected function create(array $data)
+    {
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
-         if(auth()->id){
-        Subbscription::create([
-            'name' => $package->name,
-            'payment' => 0,
-            'trial_ends_at' => Carbon::now()->addMonths(1),
-            'ends_at' => $this->package_validity($package->validity),
-            'user_id' => auth()->id,
-            
-        ]);
-        }
-       
     }
-
-    public function package_validity($validity) {
-        $package_validity = array();
-        switch ($validity) {
-            case 1:
-                $package_validity['package'] = "Trial Package";
-                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
-                break;
-            case 3:
-                $package_validity['package'] = "Silver Package";
-                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
-                break;
-            case 6:
-                $package_validity['package'] = "Gold Package";
-                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
-                break;
-            case 12:
-                $package_validity['package'] = "Classic Package";
-                $package_validity['ends_at'] = Carbon::now()->addMonths($validity);
-                break;
-        }
-        return $package_validity;
-    }
-
 }
